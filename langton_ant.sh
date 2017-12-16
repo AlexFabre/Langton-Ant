49fc5b80ad8297277ec37c0315951f08a9fc07aa
Alex Fabre <m.alexfabre@gmail.com>
2017-12-16 20:12:07 +0100

    Added source files, and shell script

---
+++ langton_ant.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo 'Compilation'
+gcc -o ./langton_ant.out ./src/*.c
+echo 'Execution'
+if [[ $1 -lt 11 ]]; then
+	BOARD=20
+else
+	BOARD=$1
+fi
+if [[ $2 -lt 1 ]]; then
+	TIME=60
+else
+	TIME=$2
+fi
+if [[ $TIME -lt 100 ]]; then
+	LOOP=0
+	SPEED=20
+elif [[ $TIME -lt 1000 ]]; then
+	LOOP=1
+	SPEED=10
+else
+	LOOP=1
+	SPEED=1
+fi
+./langton_ant.out $BOARD $TIME
+echo 'GIF creation'
+CMD="convert -delay $SPEED -loop $LOOP `ls -v *.pgm` langton_ant.gif"
+eval $CMD
+echo 'Cleaning repo'
+rm -R *.pgm
+echo 'Terminated'
